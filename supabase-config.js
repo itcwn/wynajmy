ee9ba983fd5965816df1d79bb143c00d28711065
itcwn <52242108+itcwn@users.noreply.github.com>
2025-09-18 11:29:19 +0200

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+window.__SUPA = {
+  SUPABASE_URL: "https://zqrzdmvzrixnyvoejtty.supabase.co",   // ← podmień
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxcnpkbXZ6cml4bnl2b2VqdHR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxMDM5MTMsImV4cCI6MjA3MzY3OTkxM30.DVreHVIg-gyCCRq1699Jfe5uRsV8OmaHpUuH-63IN_U",          // ← podmień
+  GOOGLE_MAPS_API_KEY: "YOUR-GOOGLE-MAPS-API-KEY"     // ← opcjonalnie
+};
