e0f354bfd948277b9fbfa2074cfb731be0916776
itcwn <52242108+itcwn@users.noreply.github.com>
2025-10-13 12:08:01 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,7 +2,7 @@ window.__SUPA = {
   SUPABASE_URL: "https://zqrzdmvzrixnyvoejtty.supabase.co",   // ← podmień
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxcnpkbXZ6cml4bnl2b2VqdHR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxMDM5MTMsImV4cCI6MjA3MzY3OTkxM30.DVreHVIg-gyCCRq1699Jfe5uRsV8OmaHpUuH-63IN_U",          // ← podmień
   GOOGLE_MAPS_API_KEY: "AIzaSyDvy6FNiCxuE7xzngQyUCshhl0YvU6nnqM",    // ← opcjonalnie
-  STORAGE_BUCKET_FACILITY_IMAGES: "facility-images",                 // ← opcjonalnie, nazwa bucketa w Supabase Storage
+  STORAGE_BUCKET_FACILITY_IMAGES: "RentalObjectsImages",                 // ← opcjonalnie, nazwa bucketa w Supabase Storage
   // Opcjonalnie: adres backendu obsługującego synchronizację sesji.
   // BACKEND_API_URL: "https://twoj-backend.example.com",
   // BACKEND_SET_SESSION_PATH: "/api/auth/set-session",
